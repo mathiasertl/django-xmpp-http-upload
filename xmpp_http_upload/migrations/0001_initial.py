524aeba53f1d3df8c22d870a5c09998d2c224b2b
Mathias Ertl <mati@er.tl>
2015-08-23 14:50:29 +0200

    reset migrations for first deployment

--- xmpp_http_upload/migrations/0001_initial.py
+++ xmpp_http_upload/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import xmpp_http_upload.models
 
 
 class Migration(migrations.Migration):
@@ -16,10 +17,12 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('updated', models.DateTimeField(auto_now=True)),
+                ('jid', models.CharField(max_length=256)),
                 ('name', models.CharField(max_length=256)),
                 ('size', models.PositiveIntegerField()),
                 ('type', models.CharField(max_length=64, null=True, blank=True)),
-                ('file', models.FileField(null=True, upload_to=b'http_upload', blank=True)),
+                ('hash', models.CharField(max_length=64)),
+                ('file', models.FileField(null=True, upload_to=xmpp_http_upload.models.get_upload_path, blank=True)),
                 ('uploaded', models.DateTimeField(null=True, blank=True)),
             ],
         ),
