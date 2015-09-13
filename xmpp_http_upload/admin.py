a910c5042e192adc0dc144ab480e85147e90e40d
Mathias Ertl <mati@er.tl>
2015-09-13 13:23:48 +0200

    improve admin class

--- xmpp_http_upload/admin.py
+++ xmpp_http_upload/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Upload
 
-# Register your models here.
-admin.site.register(Upload)
+class UploadedListFilter(admin.SimpleListFilter):
+    title = _('Uploaded')
+    parameter_name = 'uploaded'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('2', _('Expired')),
+            ('1', _('Yes')),
+            ('0', _('No'))
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        print(value, type(value))
+        if value == '2':
+            return queryset.expired()
+        elif value == '1':
+            return queryset.uploaded()
+        elif value == '0':
+            return queryset.for_upload()
+        return queryset
+
+
+@admin.register(Upload)
+class UploadAdmin(admin.ModelAdmin):
+    ordering = ('-created', 'jid', )
+    list_filter = (UploadedListFilter, )
+    search_fields = ('jid', 'name', )
+    list_display = ('jid', 'name', 'created', )
+    list_display_links = ('jid', 'name', )
