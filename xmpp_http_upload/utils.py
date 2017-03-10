6f4112a084a80467cec84deac9f47fd23a5a2f95
Mathias Ertl <mati@er.tl>
2017-03-10 10:12:11 +0100

    add new util function to get configration for a given JID

---
+++ xmpp_http_upload/utils.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-xmpp-http-upload
+# (https://github.com/mathiasertl/django-xmpp-http-upload).
+#
+# django-xmpp-http-upload is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# django-xmpp-http-upload is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# django-xmpp-http-upload.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import re
+
+from django.conf import settings
+from django.utils import six
+
+_acls = getattr(settings, 'XMPP_HTTP_UPLOAD_ACCESS', (('.*', False), ))
+
+
+def get_config(jid):
+    """Get the configuration for the given JID based on XMPP_HTTP_UPLOAD_ACCESS.
+
+    If the JID does not match any rule, ``False`` is returned.
+    """
+
+    for regex, config in _acls:
+        if isinstance(regex, six.string_types):
+            regex = [regex]
+
+        for subex in regex:
+            if re.search(subex, jid):
+                return config
+
+    return False
