58993e168a7bf7092c7e9a298af2dc5d81e0086f
Mathias Ertl <mati@er.tl>
2017-03-10 15:42:17 +0100

    set app_name

--- xmpp_http_upload/urls.py
+++ xmpp_http_upload/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'xmpp_http_upload'
 urlpatterns = [
     url(r'^slot/$', views.RequestSlotView.as_view(), name='slot'),
     url(r'^max_size/$', views.MaxSizeView.as_view(), name='max_size'),
