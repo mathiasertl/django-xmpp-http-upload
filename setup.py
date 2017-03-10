9cb2746c3d6765b76eb0d38303b95a005f22b8c7
Mathias Ertl <mati@er.tl>
2017-03-10 09:10:39 +0100

    add test/code_quality commands

--- setup.py
+++ setup.py
@@ -14,16 +14,97 @@
 # You should have received a copy of the GNU General Public License along with
 # django-xmpp-http-upload.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import unicode_literals
+import os
+import subprocess
+import sys
 
+from distutils.cmd import Command
 from setuptools import find_packages
 from setuptools import setup
 
+_rootdir = os.path.dirname(os.path.realpath(__file__))
 version = '0.4.1'
 requires = [
     'Django>=1.8',
     'djangorestframework>=3.5',
 ]
+
+
+class BaseCommand(Command):
+    user_options = [
+        ('suite=', None, 'Testsuite to run', )
+    ]
+
+    def initialize_options(self):
+        self.suite = ''
+
+    def finalize_options(self):
+        pass
+
+    def run_tests(self):
+        work_dir = os.path.join(_rootdir, 'demo')
+        os.chdir(work_dir)
+        sys.path.insert(0, work_dir)
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
+
+        import django
+        django.setup()
+
+        suite = 'xmpp_http_upload'
+        if self.suite:
+            if self.suite.startswith('xmpp_http_upload'):
+                suite = self.suite
+            else:
+                suite += '.tests.%s' % self.suite
+
+        from django.core.management import call_command
+        call_command('test', suite)
+
+
+class TestCommand(BaseCommand):
+    description = 'Run the test-suite for django-xmpp-http-upload'
+
+    def run(self):
+        self.run_tests()
+
+
+class QualityCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        proj = 'xmpp_http_upload'
+        print('isort --check-only --diff -rc %s setup.py' % proj)
+        status = subprocess.call(['isort', '--check-only', '--diff', '-rc', proj, 'setup.py'])
+        if status != 0:
+            sys.exit(status)
+
+        print('flake8 %s fabfile.py setup.py' % proj)
+        status = subprocess.call(['flake8', proj, 'setup.py'])
+        if status != 0:
+            sys.exit(status)
+
+        work_dir = os.path.join(_rootdir, proj)
+
+        os.chdir(work_dir)
+        sys.path.insert(0, work_dir)
+
+        import django
+
+        # This does not import settings.py but instead loads our own settings
+        from django.conf import settings
+        settings.configure(DEBUG=True)
+        django.setup()
+
+        from django.core.management import call_command
+        print('python ca/manage.py check')
+        call_command('check')
+
 
 setup(
     name='django-xmpp-http-upload',
@@ -35,6 +116,11 @@ setup(
     install_requires=requires,
     license="GNU General Public License (GPL) v3",
     packages=find_packages(),
+    cmdclass={
+        # 'coverage': CoverageCommand,
+        'test': TestCommand,
+        'code_quality': QualityCommand,
+    },
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
